49b19f55e37aceab5cd915894ea030795c876a1d
Marcelo da Torre <marcelodatorre28Qgmail.com>
2022-06-12 10:27:05 -0300

    ya estamos listos para subir a produccion

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Nuestro primer servidor de Node , con npm e instalacion de Git y nos dimos de alta
 # en github
 # //https://www.youtube.com/watch?v=PBe4zuRDy04&list=PLAmcNbGd0fkNIA3xFAbu99xgorPm4yxFX&index=4
+# https://www.youtube.com/watch?v=xmZjvbttwr0&list=PLAmcNbGd0fkNIA3xFAbu99xgorPm4yxFX&index=6
+# https://www.youtube.com/watch?v=hf40uuRjeOE&list=PLAmcNbGd0fkNIA3xFAbu99xgorPm4yxFX&index=7
+# https://www.youtube.com/watch?v=nx0giCbyrkk&list=PLAmcNbGd0fkNIA3xFAbu99xgorPm4yxFX&index=8
 
 hay bastante info y es sencillo
--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 
 const express = require('express')
-const { ppid } = require('process')
+const multer = require ('multer')
+const sharp = require ('sharp')
+const fs = require ('fs')
+
+const storageStrategy = multer.memoryStorage()
+const upload = multer({ storage :storageStrategy })
+
+
 const app = express()
-app.get('/',function(re,res){
-    res.send('hola mundo')
+app.use(express.json())
+
+app.get('/',function(req,res){
+    res.send('hola mundo!!!')
 })
-app.listen(3000)
+
+app.post('/imagen',upload.single('imagen'), async function (req,res){
+
+    const body = req.body
+    const imagen = req.file
+    const processedImage = sharp(imagen.buffer)
+    const resizedImage = processedImage.resize(800,200,{
+        fit:'contain',
+        background:"#fff"
+    })
+
+    const resizedImageBuffer = await resizedImage.toBuffer()
+    fs.writeFileSync ('nuevaruta/prueba.png',resizedImageBuffer)
+
+    console.log(resizedImageBuffer)
+
+    res.send ({resizeImage:resizedImageBuffer})
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,function(){
+    console.log("servidor escuchando en el puerto",PORT)
+})
